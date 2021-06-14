2d317b562e3095e6ef3b7e79795c8d71614f2619
Irupc <68991705+Irupc@users.noreply.github.com>
2021-06-14 23:10:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ def get_url():
     url = contents['url']
     return url
 
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
 def bop(bot, update):
-    url = get_url()
+    url = get_image_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
 def main():
-    updater = Updater('1659820361:AAE47U6e1yQVVWel2M1uqz2_esUYbJXMdX4')
+    updater = Updater('YOUR_TOKEN')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop',bop))
     updater.start_polling()
