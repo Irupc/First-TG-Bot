af118ee35fb4b2f35dc67af8f5da9e92d83251d6
Irupc <68991705+Irupc@users.noreply.github.com>
2021-06-20 12:37:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import Constants as keys
-from telegram.ext import *
+import random
+from telegram import Update
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+from telegram.ext import messagequeue as mq
+import telegram
+from telegram.utils.request import Request
+import logging
+import os
 import Responses as R
 
 print("Bot Started")
